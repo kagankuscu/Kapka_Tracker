3f72cd32151c8bd6304452bf509407cea5d9d58a
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-01-09 12:20:52 +0200

    Color Change

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
 Map<int, Color> color = {
-  50: Color.fromRGBO(7, 34, 39, .1),
-  100: Color.fromRGBO(7, 34, 39, .2),
-  200: Color.fromRGBO(7, 34, 39, .3),
-  300: Color.fromRGBO(7, 34, 39, .4),
-  400: Color.fromRGBO(7, 34, 39, .5),
-  500: Color.fromRGBO(7, 34, 39, .6),
-  600: Color.fromRGBO(7, 34, 39, .7),
-  700: Color.fromRGBO(7, 34, 39, .8),
-  800: Color.fromRGBO(7, 34, 39, .9),
-  900: Color.fromRGBO(7, 34, 39, 1),
+  50: Color.fromRGBO(29, 63, 114, .1),
+  100: Color.fromRGBO(29, 63, 114, .2),
+  200: Color.fromRGBO(29, 63, 114, .3),
+  300: Color.fromRGBO(29, 63, 114, .4),
+  400: Color.fromRGBO(29, 63, 114, .5),
+  500: Color.fromRGBO(29, 63, 114, .6),
+  600: Color.fromRGBO(29, 63, 114, .7),
+  700: Color.fromRGBO(29, 63, 114, .8),
+  800: Color.fromRGBO(29, 63, 114, .9),
+  900: Color.fromRGBO(29, 63, 114, 1),
 };
 
-MaterialColor materialColor = MaterialColor(0xFF072227, color);
+MaterialColor materialColor = MaterialColor(0xFF1d3f72, color);
 
-Color colorBlack = Color(0xFF072227);
-Color colorDarkBlue = Color(0xFF35858B);
-Color colorBlue = Color(0xFF4FBDBA);
-Color colorLightBlue = Color(0xFFAEFEFF);
+Color colorBlack = Color(0xFF1d3f72);
+Color colorDarkBlue = Color(0xFF5699d2);
+Color colorBlue = Color(0xFF71c2cc);
+Color colorLightBlue = Color(0xFF0acce4);
 Color colorWhite = Colors.white70;
 
 Color colorGreen = Colors.green[400]!;
